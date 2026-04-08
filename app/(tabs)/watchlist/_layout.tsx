fb31625a0ab0d36a29a17bdaff2a9b21fbec05e1
Chris <chris.popovits@student.sl.on.ca>
2026-04-08 12:31:18 -0400

    tabs navigation working, stack not

--- app/(tabs)/watchlist/_layout.tsx
+++ app/(tabs)/watchlist/_layout.tsx
@@ -2,30 +2,24 @@ import { Stack } from "expo-router";
 import { Pressable, Text } from "react-native";
 import { useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
-import { StocksProvider } from "../src/context/StocksContext"
+import { StocksProvider } from "../../../src/context/StocksContext";
 
 export default function RootLayout() {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <StocksProvider>
-            <Stack>
-                {/* HOME SCREEN */}
-                <Stack.Screen
-                    name="index"
-                    options={{
-                        headerTitle: "My Stocks",
-                        headerTitleAlign: "center",
-                        headerRight: () => (
-                        // *** Route for earnings. Can mimic for sending to news from Stock tick/detail screen
-                            <Pressable onPress={() => router.push("/earnings")}>
-                                <Ionicons name="calendar-outline" size={22} color="black" />
-                            </Pressable>
-                        ),
-                    }}
-                />
-
-            </Stack>
-        </StocksProvider>
-    );
+  return (
+    <StocksProvider>
+      <Stack>
+        {/* HOME SCREEN */}
+        <Stack.Screen
+          name="index"
+          options={{
+            headerTitle: "My Stocks",
+            headerTitleAlign: "center",
+          }}
+        />
+        <Stack.Screen name="stocktick" options={{ title: "Stock Ticker" }} />
+      </Stack>
+    </StocksProvider>
+  );
 }
