d4b9055283936e3a5df02621ba23929c2dbb23bd
Chris <chris.popovits@student.sl.on.ca>
2026-04-10 11:57:05 -0400

    misc

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,4 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   // Root uses a Stack, but the actual primary navigation is Tabs inside (tabs).
   return <Stack screenOptions={{ headerShown: false }} />;
-  /*
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
-  */
 }
